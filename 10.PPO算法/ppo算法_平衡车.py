e813ebb096cb079477b367a4863ff0d534cec84f
zxq <zxq@123.com>
2025-08-13 13:53:00 +0800

    0813

--- 10.PPO算法/ppo算法_平衡车.py
+++ 10.PPO算法/ppo算法_平衡车.py
@@ -1,5 +1,6 @@
 import random
 import gymnasium as gym
+import numpy as np
 import torch
 import torch.nn as nn
 from tqdm import tqdm
@@ -62,9 +63,16 @@ class CartPoleTrainer(object):
         self.actor = Actor(state_dim, action_dim)
         self.critic = Critic(state_dim)
 
+        self.optimizer_actor = torch.optim.Adam(self.actor.parameters(), lr=1e-3)
+        self.optimizer_critic = torch.optim.Adam(self.critic.parameters(), lr=1e-2)
+        self.loss_fn = torch.nn.MSELoss()
+
+        # 超参数
+        self.GAMMA = 0.98
+        self.LAMBDA = 0.95
+
     # 优势函数
-    @staticmethod
-    def get_advantages(deltas):
+    def get_advantages(self, deltas):
         """
         inputs: episode内部每个状态的td error
         outputs: A是td error的加权和
@@ -74,7 +82,7 @@ class CartPoleTrainer(object):
         # 反向遍历deltas
         s = 0.0  # 累计和
         for delta in deltas[::-1]:  # 逆向累加
-            s = 0.98 * 0.95 * s + delta  # 累计td error
+            s = self.GAMMA * self.LAMBDA * s + delta  # 累计td error
             advantages.append(s)  # 当前时刻的a值放在后面
 
         # 逆序
@@ -101,7 +109,7 @@ class CartPoleTrainer(object):
 
     def get_data(self):
         """
-
+        batch data: 同一个episode的多个时刻数据
         Returns:
             states: [b, 4]
             rewards: [b, 1]
@@ -138,18 +146,14 @@ class CartPoleTrainer(object):
             # 更新游戏状态,开始下一个动作
             state = next_state
 
-        states = torch.FloatTensor(states).reshape(-1, 4)  # [b, 4]
-        rewards = torch.FloatTensor(rewards).reshape(-1, 1)  # [b, 1]
-        actions = torch.LongTensor(actions).reshape(-1, 1)  # [b, 1]
-        next_states = torch.FloatTensor(next_states).reshape(-1, 4)  # [b, 4]
-        overs = torch.LongTensor(overs).reshape(-1, 1)  # [b, 1]
+        states = torch.tensor(np.array(states), dtype=torch.float32).reshape(-1, 4)  # [b, 4]
+        rewards = torch.tensor(np.array(rewards), dtype=torch.float32).reshape(-1, 1)  # [b, 1]
+        actions = torch.tensor(np.array(actions), dtype=torch.long).reshape(-1, 1)  # [b, 1]
+        next_states = torch.tensor(np.array(next_states), dtype=torch.float32).reshape(-1, 4)  # [b, 4]
+        overs = torch.tensor(np.array(overs), dtype=torch.long).reshape(-1, 1)  # [b, 1]
         return states, rewards, actions, next_states, overs
 
     def train(self):
-        optimizer_actor = torch.optim.Adam(self.actor.parameters(), lr=1e-3)
-        optimizer_critic = torch.optim.Adam(self.critic.parameters(), lr=1e-2)
-        loss_fn = torch.nn.MSELoss()
-
         # 玩N局游戏,每局游戏训练M次
         for epoch in tqdm(range(1000)):
             # 1. 玩一局游戏,得到数据
@@ -159,10 +163,13 @@ class CartPoleTrainer(object):
             # 计算values
             values = self.critic(states)  # [b, 4] -> [b, 1]
             # 计算td target: r + γ*net(s')
-            targets = self.critic(next_states).detach()  # [b, 4] -> [b, 1]
-            targets = targets * 0.98
+            targets = self.critic(next_states)  # [b, 4] -> [b, 1]
             targets *= (1 - overs)  # t = t*(1-over) 将over估计分数置为0
+
+            targets = targets * self.GAMMA
             targets += rewards  # t = r + t
+            targets = targets.detach()
+
             # [b, 1]. TD Error
             deltas = (targets - values).squeeze(dim=1).tolist()
 
@@ -174,17 +181,16 @@ class CartPoleTrainer(object):
             # [b, 2] -> [b, 1]
             old_probs = self.actor(states)  # [b, 2]
             old_probs = old_probs.gather(dim=1, index=actions)  # actions.shape=[b, 1]
-            old_probs = old_probs.detach()  # [b, 1]
+            old_probs = old_probs.detach()  # [b, 1]，这是收集的数据，不参与梯度
 
-            # 5. 每批数据反复训练10次
+            """以上是获取数据-------------------------------------------------"""
+            # 5. 每批数据反复训练10次，多次迭代，逼近目标
             for _ in range(10):
-                # 5.1 重新计算每一步动作的概率
+                # 5.1 策略损失: -min(rA, clip(r)A). r = p_new/p_old.  p_new=net(s), p_old=net(s)
+                # 求出概率的变化ratio
                 new_probs = self.actor(states)  # [b, 4] -> [b, 2]
                 new_probs = new_probs.gather(dim=1, index=actions)  # [b, 2] -> [b, 1]
-                new_probs = new_probs  # [b, 1]
-
-                # 5.2 策略损失
-                # 求出概率的变化ratio
+                # new_probs = new_probs  # [b, 1]
                 ratios = new_probs / old_probs  # [b, 1] - [b, 1] -> [b, 1]
 
                 # 计算截断的和不截断的两份loss,取其中小的
@@ -193,19 +199,19 @@ class CartPoleTrainer(object):
                 loss_actor = -torch.min(surr1, surr2)  # [b, 1] 每个样本的策略损失
                 loss_actor = loss_actor.mean()  # batch平均策略损失
 
-                # 5.3 价值损失
+                # 5.2 价值损失
                 # 必须重新计算value, 并计算时序差分loss
-                values = self.critic(states)  # 在不同的net计算loss，这里是loss_critic
-                loss_critic = loss_fn(values, targets)  # loss = net(s) - (r + γ*net(s'))
+                values = self.critic(states)  # 在不同的net计算values，这里是critic_net. 而targets是本来就有的。
+                loss_critic = self.loss_fn(values, targets)  # loss = net(s) - (r + γ*net(s'))
 
                 # 5.4 更新参数
-                optimizer_actor.zero_grad()
+                self.optimizer_actor.zero_grad()
                 loss_actor.backward()
-                optimizer_actor.step()
+                self.optimizer_actor.step()
 
-                optimizer_critic.zero_grad()
+                self.optimizer_critic.zero_grad()
                 loss_critic.backward()
-                optimizer_critic.step()
+                self.optimizer_critic.step()
 
             if epoch % 50 == 0:
                 test_result = sum([self.test() for _ in range(10)]) / 10
--- 10.PPO算法/tmp-ppo.py
+++ 10.PPO算法/tmp-ppo.py
@@ -1,7 +1,10 @@
+import random
+
 import gymnasium as gym
+import numpy as np
 import torch
 import torch.nn as nn
-
+from tqdm import tqdm
 
 """
 Actor: p = actor_net(s)
@@ -49,4 +52,175 @@ class Critic(nn.Module):
 
         """
         return self.layer(state)
+
+
+class CartPoleTrainer(object):
+    def __init__(self):
+        # env
+        self.env = gym.make("CartPole-v1")
+
+        # model
+        state_dim = self.env.observation_space.shape[0]
+        self.action_dim = self.env.action_space.n
+        self.actor_net = Actor(state_dim, self.action_dim)
+        self.critic_net = Critic(state_dim)
+
+        self.optimizer_actor = torch.optim.Adam(self.actor_net.parameters(), lr=1e-3)
+        self.optimizer_critic = torch.optim.Adam(self.critic_net.parameters(), lr=1e-2)
+        self.loss_fn = torch.nn.MSELoss()
+
+        # 超参数
+        self.BATCH_SIZE = 128
+
+    def get_action(self, state):
+        """
+
+        Args:
+            state: array. (4, )
+
+        Returns:
+
+        """
+        state_t = torch.tensor(state, dtype=torch.float32).reshape(1, 4)
+        prob = self.actor_net(state_t)
+        action = random.choices(range(self.action_dim), weights=prob[0].tolist(), k=1)[0]
+        return action
+
+    def get_data(self):
+        """
+        Returns:
+            states: [b, 4]
+            rewards: [b, 1]
+            actions: [b, 1]
+            next_states: [b, 4]
+            overs: [b, 1]
+        """
+        states, actions, rewards, overs, next_states = [], [], [], [], []
+        over = False
+        state, _ = self.env.reset()
+        while not over:
+            action = self.get_action(state)
+            next_state, reward, terminate, truncated, info = self.env.step(action)
+            over = terminate or truncated
+            # 记录
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
+            overs.append(over)
+            next_states.append(next_state)
+            # next
+            state = next_state
+        # tensor
+        states = torch.tensor(np.array(states), dtype=torch.float32).reshape(-1, 4)  # [b, 4]
+        rewards = torch.tensor(np.array(rewards), dtype=torch.float32).reshape(-1, 1)  # [b, 1]
+        actions = torch.tensor(np.array(actions), dtype=torch.long).reshape(-1, 1)  # [b, 1]
+        next_states = torch.tensor(np.array(next_states), dtype=torch.float32).reshape(-1, 4)  # [b, 4]
+        overs = torch.tensor(np.array(overs), dtype=torch.long).reshape(-1, 1)  # [b, 1]
+        return states, actions, rewards, overs, next_states
+
+
+    def train(self):
+
+        for epoch in tqdm(range(1500)):
+            # 1. 获取batch data
+            states, actions, rewards, overs, next_states = self.get_data()
+
+            # 2. target error
+            # values
+            values = self.critic_net(states)  # (128,4) -> (128,1)
+
+            # TD target
+            target_values = self.critic_net(next_states)  # (128,4) -> (128,1)
+            target_values *= (1 - overs)
+            target_values = rewards + 0.98*target_values
+            target_values = target_values.detach()
+
+            # target error: delta
+            deltas = target_values - values  # (128, 1) error = r + net(s') - net(s)
+
+            # 3. advantage
+            advantages = self.get_advantage(deltas)  # (128, 1) -> list of float
+            advantages = torch.tensor(advantages, dtype=torch.float32).reshape(-1, 1)  # (128,1)
+
+            # 4. 取出每一步动作的概率
+            old_probs = self.actor_net(states)  # [b, 2]
+            old_probs = old_probs.gather(dim=1, index=actions)  # actions.shape=[b, 1]
+            old_probs = old_probs.detach()
+
+            # 多次迭代，逼近目标
+            for i in range(10):
+                # 4. policy loss
+                # 求出概率的变化ratio
+                new_probs = self.actor_net(states)  # (128,4) -> (128,2)
+                new_probs = new_probs.gather(dim=1, index=actions)  # (128,1)
+                ratios = new_probs / old_probs  # (128,1)
+
+                surr1 = ratios * advantages
+                surr2 = torch.clamp(ratios, 0.8, 1.2) * advantages
+                loss_actor = -torch.min(surr1, surr2)  # (128,1)
+                loss_actor = loss_actor.mean()  #
+
+                # 5. value loss
+                # values
+                vals = self.critic_net(states)  # (128,4) -> (128,1)
+                # target values
+                vals_loss = self.loss_fn(vals, target_values)
+
+                self.optimizer_actor.zero_grad()
+                loss_actor.backward()
+                self.optimizer_actor.step()
+
+                self.optimizer_critic.zero_grad()
+                vals_loss.backward()
+                self.optimizer_critic.step()
+
+            if epoch % 50 == 0:
+                test_result = sum([self.test() for _ in range(10)]) / 10
+                print(epoch, test_result)
+
+    def test(self):
+        # 初始化游戏
+        state, _ = self.env.reset()
+
+        # 记录反馈值的和,这个值越大越好
+        reward_sum = 0
+
+        # 玩到游戏结束为止
+        over = False
+        while not over:
+            # 根据当前状态得到一个动作
+            # action = self.get_action_by_max_p(state)
+            action = self.get_action(state)
+            # 执行动作,得到反馈
+            state, reward, terminate, truncated, _ = self.env.step(action)
+            over = terminate or truncated
+            reward_sum += reward
+
+        return reward_sum
+
+    def get_advantage(self, deltas):
+        """
+
+        Args:
+            deltas: tensor. (128,1)
+
+        Returns: list of float.
+
+        """
+        advantages = []
+
+        s = 0
+        for i in reversed(range(deltas.shape[0])):
+            s = 0.98*0.95*s + deltas[i]
+            advantages.append(s)
+
+        advantages.reverse()
+        return advantages
+
+
+if __name__ == '__main__':
+    trainer = CartPoleTrainer()
+    trainer.train()
+
+
 
